212141807dec719e1652a261eab6c3614dcac7c2
Viktor Szépe <viktor@szepe.net>
2020-12-24 08:45:19 +0100

    Improve DocHooks::check

--- src/Checker/DocHooks.php
+++ src/Checker/DocHooks.php
@@ -27,23 +27,23 @@ class DocHooks extends Abstracts\Checker {
 	 *
 	 * @since  1.0.0
 	 * @throws \Exception When provided value is not a string or numeric.
-	 * @param  string $value If dochooks should be enabled or disabled.
+	 * @param  mixed $enabled If dochooks should be enabled or disabled.
 	 * @return void
 	 */
-	public function check( $value ) {
+	public function check( $enabled ) {
 
-		if ( ! is_bool( $value ) ) {
+		if ( ! is_bool( $enabled ) ) {
 			throw new \Exception( 'DocHooks Check requires bool parameter' );
 		}
 
-		$reflector = new \ReflectionClass( $this );
-		$comment   = $reflector->getMethod( 'check' )->getDocComment();
+		$reflector   = new \ReflectionClass( $this );
+		$has_comment = false !== strpos( $reflector->getMethod( 'check' )->getDocComment(), '@dochooks-test' );
 
-		if ( false === strpos( $comment, '@dochooks-test' ) && $value ) {
+		if ( ! $has_comment && $enabled ) {
 			$this->add_error( __( 'Support for DocHooks is required', Requirements::$textdomain ) );
 		}
 
-		if ( 0 <= strpos( $comment, '@dochooks-test' ) && ! $value ) {
+		if ( $has_comment && ! $enabled ) {
 			$this->add_error( __( 'Support for DocHooks is superfluous', Requirements::$textdomain ) );
 		}
 
